92c3cbb2ddd9a8e125f2b4e50152d36e44b51025
Kamile Seidu <seidukamile@gmail.com>
2024-07-09 00:43:27 -0700

    added some styles and layout to categories component

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,21 +1,39 @@
 import classes from "../components/Categories.module.css";
 import arrowRight from "../assets/right-arrow-svgrepo-com.svg";
+import rec2 from "../assets/rec_2.png";
+import rec4 from "../assets/rec_4.jpg";
+import rec3 from "../assets/rec_3.jpg";
 
 export default function Categories() {
   return (
     <div>
-      <span>
-        <h4>Categories</h4>
-        <h5>Explore our Top Categories</h5>
-      </span>
-
       <div>
-        <button>
-          <img src={arrowRight} alt="" />
-        </button>
-        <button>
-          <img src={arrowRight} alt="" />
-        </button>
+        <span>
+          <h4>Categories</h4>
+          <h5>Explore our Top Categories</h5>
+        </span>
+        <div>
+          <button className={classes.selected}>
+            <img src={arrowRight} alt="" />
+          </button>
+          <button>
+            <img src={arrowRight} alt="" />
+          </button>
+        </div>
+      </div>
+      <div className={classes.ad}>
+        <span>
+          <img src={rec2} alt="" />
+          <h2>Higher Education</h2>
+        </span>
+        <span>
+          <img src={rec3} alt="" />
+          <h2>Management Books</h2>
+        </span>
+        <span>
+          <img src={rec4} alt="" />
+          <h2>Engineering Books</h2>
+        </span>
       </div>
     </div>
   );
